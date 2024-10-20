b53bbdc054f9be9e22f027f47345ebe4fd7e0c27
ImranParthib <imranparthib13@gmail.com>
2024-10-20 23:41:09 +0600

    🚑 Refactor import statements in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,8 @@ import About from "./components/About";
 import Services from "./components/Services";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
-import Footer from "./components/Footer";  
+import Footer from "./components/Footer";
+
 
 function App() {
   return (
