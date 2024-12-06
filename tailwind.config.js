3d5ee44c09be22c1252b0d014ed6ff0754011369
ImranParthib <imranparthib13@gmail.com>
2024-12-07 01:53:29 +0600

    🚨 Refactor Tailwind configuration: switch to ES module syntax and streamline content array

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  darkMode: 'class', // Enable dark mode
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class", // Enable dark mode
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
